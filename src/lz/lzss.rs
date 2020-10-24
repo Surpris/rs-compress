7e656a102d1cdab6eaddffcf0e40b44437fc0c9f
Toshiyuki Nishiyama Hiraki <take90-it09-easy27@outlook.jp>
2020-10-24 18:29:03 +0900

    implementing lzss

--- src/lz/lzss.rs
+++ src/lz/lzss.rs
@@ -2,6 +2,130 @@
 //!
 //!
 
+use std::collections::HashMap;
+
+// length of bits expressing the length of target
+const LEN_OF_BITS: u64 = 4;
+// minimum length of target to encode
+const MIN_LEN_OF_TARGET: usize = 3;
+// maximum length of target to encode
+// = MIN_LEN_OF_TARGET + 2**LEN_OF_BITS - 1
+const MAX_LEN_OF_TARGET: usize = 18;
+// length of bits expressin the position of an encoding slide
+const POSITION_BITS: u64 = 13;
+// length of window
+const WINDOW_LEN: usize = 1 << POSITION_BITS;
+// limit of window size
+const WINDOW_LIMIT: usize = WINDOW_LEN * 2;
+// buffer size
+const NIL: usize = WINDOW_LIMIT + MAX_LEN_OF_TARGET;
+
+/// get the hash value corresponding to the starting point of encoding
+/// 
+/// # Parameters
+/// * buff: buffer for encoding
+/// * start_point: starting point of encoding
+fn get_hash_value(buff: &[u8], start_point: usize) -> u64 {
+    let mut value: u64 = 0u64;
+    for ii in 0..MIN_LEN_OF_TARGET {
+        value = (value << 8) + buff[start_point + ii] as u64;
+    }
+    value
+}
+
+/// insert a recode including the starting point to a hash table
+/// 
+/// # Parameters
+/// * buff: buffer for encoding
+/// * next_pos: connected list of the starting points
+/// * ht: hash table of the starting points
+/// * start_point: starting point of encoding
+fn insert_recode(
+    buff: &[u8],
+    next_pos: &mut [usize],
+    ht: &mut HashMap<u64, usize>,
+    start_point: usize,
+) {
+    let hv: u64 = get_hash_value(buff, start_point);
+    match ht.get(&hv) {
+        Some(x) if *x != NIL => next_pos[start_point & (WINDOW_LEN - 1)] = *x,
+        _ => next_pos[start_point & (WINDOW_LEN - 1)] = NIL,
+    }
+    ht.insert(hv, start_point);
+}
+
+/// get the matched pattern
+/// 
+/// # Parameters
+/// * buff
+/// * next_pos
+/// * ht
+/// * data_size
+/// * start_point
+/// * match_len
+/// * match_pos
+fn get_matched_pattern(
+    buff: &[u8],
+    next_pos: &[usize],
+    ht: &HashMap<u64, usize>,
+    data_size: usize,
+    start_point: usize,
+    match_len: &mut usize,
+    match_pos: &mut usize,
+) {
+    let hv: u64 = get_hash_value(buff, start_point);
+    let limit: usize = if start_point < WINDOW_LEN { 0 } else {start_point - WINDOW_LEN};
+    if let Some(matched) = ht.get(&hv) {
+        let mut nbr = *matched;
+        while nbr != NIL && nbr >= limit {
+            if buff[start_point + *match_len] == buff[nbr + *match_len] {
+                let mut x: usize = 0;
+                while x < MAX_LEN_OF_TARGET {
+                    if buff[start_point + x] != buff[nbr + x] {
+                        break;
+                    }
+                    x += 1;
+                }
+                if *match_len < x {
+                    *match_len = x;
+                    *match_pos = nbr;
+                    if x == MAX_LEN_OF_TARGET {
+                        break;
+                    }
+                }
+            }
+            nbr = next_pos[nbr & (WINDOW_LEN - 1)];
+        }
+        if *match_len != 0 && *match_len >= data_size - start_point {
+            *match_len = data_size - start_point;
+        }
+    }
+}
+
+/// update inner parameters
+/// 
+/// # Parameters
+/// * src
+/// * buff
+/// * next_pos
+/// * ht
+/// * data_size
+/// * start_point
+/// * match_len
+/// * match_pos
+fn update_values(
+    src: &[u8],
+    buff: &mut [u8],
+    next_pos: &mut [usize],
+    ht: &mut HashMap<u64, usize>,
+    data_size: &mut usize,
+    start_point: &mut usize,
+    match_len: &mut usize,
+    match_pos: &mut usize,
+) {
+
+}
+
 pub fn encode(src: &[u8]) -> Vec<bool> {
     Vec::new()
 }
