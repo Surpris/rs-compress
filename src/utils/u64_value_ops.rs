d9dd9ed19eba48d3e043add107f06d0fdbe5e368
Toshiyuki Nishiyama Hiraki <take90-it09-easy27@outlook.jp>
2020-10-24 22:42:21 +0900

    edit u64_value_ops.rs

--- src/utils/u64_value_ops.rs
+++ src/utils/u64_value_ops.rs
@@ -1,10 +1,10 @@
-//! u32_value_ops
+//! u64_value_ops
 //!
 //! functions for a usize (array)
 
 use std::mem::size_of;
 
-/// convert a byte array into a bit array
+/// convert a u64-integer array into a bit array
 pub fn ary_to_bits(src: &[u64]) -> Vec<bool> {
     let mut dst: Vec<bool> = Vec::new();
     for v in src {
@@ -13,12 +13,12 @@ pub fn ary_to_bits(src: &[u64]) -> Vec<bool> {
     dst
 }
 
-/// convert a byte into a bit array
+/// convert a u64 integer into a bit array
 pub fn to_bits(src: u64) -> Vec<bool> {
     to_n_bits(src, 8 * size_of::<u64>() as u64)
 }
 
-/// convert a byte into a bit array
+/// convert a u64 integer into a bit array
 /// with the length of length
 pub fn to_n_bits(src: u64, length: u64) -> Vec<bool> {
     let mut dst: Vec<bool> = Vec::new();
