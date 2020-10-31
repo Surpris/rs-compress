d435d6d0166970db9b7938de89f6e629879679c9
Toshiyuki Nishiyama Hiraki <take90-it09-easy27@outlook.jp>
2020-11-01 08:54:38 +0900

    add a function to get the nbr of bits for a value

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -16,3 +16,17 @@ where
 {
     U::from(x).unwrap()
 }
+
+pub fn bit_length_of_value<T, U>(v: T) -> U
+where
+    T: PrimInt,
+    U: PrimInt,
+{
+    let mut p: u64 = 0;
+    let mut q: u64 = (cast_t2u::<T, u64>(v) + 1) >> 1;
+    while q > 0 {
+        p += 1;
+        q >>= 1;
+    }
+    cast_t2u(p)
+}
